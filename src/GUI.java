afc6a60e2b7181969c82c329433c9e8c3d89f88e
RensMD <doornbuschrm@gmail.com>
2016-06-06 12:09:35 +0200

    Start

---
+++ src/GUI.java
@@ -0,0 +1,113 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+/**
+ * Created by Rens on 2-6-2016.
+ */
+
+public class GUI {
+
+    private JButton fileLocationButton;
+    private JButton gripUpButton;
+    private JButton gripDownButton;
+    private JButton startButton;
+
+    private boolean startButtonStop = false;
+
+    public static String selectedForce;
+    public static String selectedSpeed;
+
+    private String[] forceStrings = {"100Kg", "500Kg"};
+    private String[] speedStrings = {"10 mm/min", "20 mm/min", "30 mm/min", "40 mm/min"};
+
+    JComboBox<String> forceComboBox = new JComboBox<>(forceStrings);
+    JComboBox<String> speedComboBox = new JComboBox<>(speedStrings);
+
+    public GUI() {
+        // File location
+        fileLocationButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                super.mouseClicked(mouseEvent);
+                // TODO: fileLocationSelect();
+            }
+        });
+
+        // Force
+        // TODO: action listener right for Combo box?
+        forceComboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JComboBox<String> forceComboBox = (JComboBox<String>) actionEvent.getSource();
+                String selectedForce = (String) forceComboBox.getSelectedItem();
+            }
+        });
+
+        // Speed
+        speedComboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JComboBox<String> speedComboBox = (JComboBox<String>) actionEvent.getSource();
+                String selectedSpeed = (String) speedComboBox.getSelectedItem();
+            }
+        });
+
+        // Control
+        // Up
+        gripUpButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent mouseEvent) {
+                super.mousePressed(mouseEvent);
+                // TODO: Write("A");
+            }
+        });
+        gripUpButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent mouseEvent) {
+                super.mouseReleased(mouseEvent);
+                // TODO: Write("H");
+            }
+        });
+
+        // Down
+        gripDownButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent mouseEvent) {
+                super.mousePressed(mouseEvent);
+                // TODO: Write("B");
+            }
+        });
+        gripDownButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseReleased(MouseEvent mouseEvent) {
+                super.mouseReleased(mouseEvent);
+                // TODO: Write("G");
+            }
+        });
+
+        // Start
+        startButton.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent mouseEvent) {
+                super.mouseClicked(mouseEvent);
+                if (startButtonStop) {
+                    Main.stopNow = true;
+                    startButton.setText("Stopped!");
+                }
+                else{
+                    Main.start();
+                    startButtonStop = true;
+                    startButton.setText("Stop");
+                }
+            }
+        });
+    }
+}
+
+
+
+
+
---
+++ src/Main.java
@@ -0,0 +1,72 @@
+import java.io.*;
+import java.util.Objects;
+
+/**
+ * Created by Rens on 2-6-2016.
+ */
+public class Main {
+    String com = null;
+    String filelocation = null;
+    String filename = null;
+
+    static String elongation; //TODO: VBFixedString
+    String elongationPrint;
+    String force = null;
+    Double time = null;
+    Double pwmsignal = null;
+    Float velocity = null;
+
+    static Boolean stopNow = false;
+
+
+    // TODO: file
+
+    public Main(){
+        // TODO: at start do:
+    }
+
+    public static void start() {
+
+        //if(filelocation == ""){
+        // select file location }
+
+
+        if (GUI.selectedForce.equals("100Kg")){
+            //TODO: Write("E");
+        }
+        else{
+            //TODO: Write("F");
+        }
+
+        if (GUI.selectedSpeed.equals("10 mm/min")){
+            //TODO: Write("1");
+        }
+        else if (GUI.selectedSpeed.equals("20 mm/min")){
+            //TODO: Write("2");
+        }
+        else if (GUI.selectedSpeed.equals("50 mm/min")){
+            //TODO: Write("3");
+        }
+        else{
+            //TODO: Write("4");
+        }
+
+        //TODO: Write("I"); to start
+
+        //TODO: procedure loop
+
+        // Check for cancel
+        if(stopNow || Objects.equals(elongation, "a")){
+            //Write("I");
+            // Stop timer
+        }
+        else{
+
+
+        }
+
+
+
+    }
+
+}

d10277c40e09dcbba35dd6e794c6de9b0a0acc22
RensMD <doornbuschrm@gmail.com>
2016-06-14 12:03:45 +0200

    Cleanup

--- src/GUI.java
+++ src/GUI.java
@@ -1,49 +1,39 @@
 import java.io.*;
+import java.nio.file.Paths;
 
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 
 import javax.swing.*;
-import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.Vector;
 
 /**
  * Created by Rens Doornbusch on 2-6-2016. *
+ * Inspired by the LETT Visual Basic code of Pieter Welling *
+ * Created to enable cross platform (X) usage of application for LETT Desktop tests *
+ * Extends the functionality with compression test *
  */
 
-public class GUI extends JPanel implements Network_iface, ActionListener {
+public class GUI extends JPanel implements Network_iface {
 
-    //String com = null;
-    // String filename = null;
     //String elongationPrint;
     //String force = null;
     //Double time = null;
-    //Double pwmsignal = null;
+    //Double pwmSignal = null;
     //Float velocity = null;
 
-    private static String fileLocation = null;
-
     static private final String newline = "\n";
-    private static int speed = 19200;
     private static Network network;
 
     private JPanel LettxJpanel;
 
-    private JButton fileLocationButton;
     private JButton gripUpButton;
     private JButton gripDownButton;
     private JButton startButton;
 
-    //private JButton openButton, saveButton;
-    private JTextArea log;
-    private JFileChooser fc;
-
     private boolean startButtonStop = false;
-    //private static Boolean resend_active = false;
     private static Boolean stopNow = false;
     private static String elongation; //TODO: VBFixedString
 
@@ -53,9 +43,13 @@ public class GUI extends JPanel implements Network_iface, ActionListener {
     JComboBox<String> forceComboBox;
     JComboBox<String> speedComboBox;
     JComboBox<String> testComboBox;
-    JTextField fileNameField;
-    private static String fileName;
 
+    private JButton fileLocationButton;
+    JTextField fileNameField;
+    private static String fileLocation = null;
+
+    private JFileChooser fc;
+    private JTextArea log;
 
     public static void main(String[] args) {
 
@@ -67,22 +61,12 @@ public class GUI extends JPanel implements Network_iface, ActionListener {
         frame.pack();
         frame.setVisible(true);
 
-        //Schedule a job for the event dispatch thread:
-        //creating and showing this application's GUI.
+        // Schedule a job for the event dispatch thread:
+        // creating and showing this application's GUI.
         SwingUtilities.invokeLater(() -> {
-            //Turn off metal's use of bold fonts
+            // Turn off metal's use of bold fonts
             UIManager.put("swing.boldMetal", Boolean.FALSE);
         });
-
-        // reading the speed if
-        if (args.length > 0) {
-            try {
-                speed = Integer.parseInt(args[0]);
-            } catch (NumberFormatException e) {
-                System.out.println("the speed must be an integer\n");
-                System.exit(1);
-            }
-        }
 
         // initializing reader from command line
         int i, inp_num = 0;
@@ -104,6 +88,7 @@ public class GUI extends JPanel implements Network_iface, ActionListener {
             System.out.println("    " + Integer.toString(i + 1) + ":  " + ports.elementAt(i));
         }
 
+        //TODO: Create arduino choice window
         boolean valid_answer = false;
         if(ports.size()!=1) {
             while (!valid_answer) {
@@ -128,6 +113,7 @@ public class GUI extends JPanel implements Network_iface, ActionListener {
         }
 
         // connecting to the selected port
+        int speed = 19200;
         if (network.connect(ports.elementAt(inp_num - 1), speed)) {
             System.out.println();
         } else {
@@ -135,41 +121,15 @@ public class GUI extends JPanel implements Network_iface, ActionListener {
             System.exit(1);
         }
 
-//        // asking whether user wants to mirror traffic
-//        System.out.println("do you want this tool to send back all the received messages?");
-//        valid_answer = false;
-//        while (!valid_answer) {
-//            System.out.println("'y' for yes or 'n' for no: ");
-//            try {
-//                input = in_stream.readLine();
-//                switch (input) {
-//                    case "y":
-//                        resend_active = true;
-//                        valid_answer = true;
-//                        break;
-//                    case "n":
-//                        valid_answer = true;
-//                        break;
-//                    case "q":
-//                        System.out.println("example terminated\n");
-//                        System.exit(0);
-//                }
-//            } catch (IOException e) {
-//                System.out.println("there was an input error\n");
-//                System.exit(1);
-//            }
-//        }
-
+        //TODO: Get rid of 0-254
         // reading in numbers (bytes) to be sent over the serial port
-
         System.out.println("type 'q' to end the example");
         while (true) {
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException ignored) {
             }
-//            catch (InterruptedException e1) {
-//            }
+
             System.out.println("\nenter a number between 0 and 254 to be sent ('q' to exit): ");
             try {
                 input = in_stream.readLine();
@@ -198,60 +158,14 @@ public class GUI extends JPanel implements Network_iface, ActionListener {
 
         super(new BorderLayout());
 
-        //Create the log first, because the action listeners
-        //need to refer to it.
+        //Create the log first, because the action listeners need to refer to it.
         log = new JTextArea(5,20);
         log.setMargin(new Insets(5,5,5,5));
         log.setEditable(false);
-//        JScrollPane logScrollPane = new JScrollPane(log);
 
         //Create a file chooser
         fc = new JFileChooser();
         fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
-
-//        //Create the open button.
-//        openButton = new JButton("Open a File...");
-//        openButton.addActionListener(this);
-//
-//        //Create the save button.
-//        saveButton = new JButton("Save a File...");
-//        saveButton.addActionListener(this);
-
-//        //For layout purposes, put the buttons in a separate panel
-//        JPanel buttonPanel = new JPanel(); //use FlowLayout
-//        buttonPanel.add(openButton);
-//        buttonPanel.add(saveButton);
-//
-//        //Add the buttons and the log to this panel.
-//        add(buttonPanel, BorderLayout.PAGE_START);
-//        add(logScrollPane, BorderLayout.CENTER);
-
-        //Test
-        testComboBox.addActionListener(actionEvent -> {
-            JComboBox testComboBox = (JComboBox) actionEvent.getSource();
-            testString_Current = (String) testComboBox.getSelectedItem();
-        });
-
-        // Force
-        forceComboBox.addActionListener(actionEvent -> {
-            JComboBox forceComboBox = (JComboBox) actionEvent.getSource();
-            forceString_Current = (String) forceComboBox.getSelectedItem();
-        });
-//        forceComboBox.addActionListener(new ActionListener() {
-//            @Override
-//            public void actionPerformed(ActionEvent actionEvent) {
-//                JComboBox forceComboBox = (JComboBox) actionEvent.getSource();
-//                forceString_Current = (String) forceComboBox.getSelectedItem();
-//            }
-//        });
-
-        // Speed
-        speedComboBox.addActionListener(actionEvent -> {
-            JComboBox speedComboBox = (JComboBox) actionEvent.getSource();
-            speedString_Current = (String) speedComboBox.getSelectedItem();
-        });
-
-        fileNameField.addActionListener(this);
 
         // File location
         fileLocationButton.addMouseListener(new MouseAdapter() {
@@ -274,6 +188,23 @@ public class GUI extends JPanel implements Network_iface, ActionListener {
             }
         });
 
+        // Selection Boxes
+        //Test
+        testComboBox.addActionListener(actionEvent -> {
+            JComboBox testComboBox = (JComboBox) actionEvent.getSource();
+            testString_Current = (String) testComboBox.getSelectedItem();
+        });
+        // Force
+        forceComboBox.addActionListener(actionEvent -> {
+            JComboBox forceComboBox = (JComboBox) actionEvent.getSource();
+            forceString_Current = (String) forceComboBox.getSelectedItem();
+        });
+        // Speed
+        speedComboBox.addActionListener(actionEvent -> {
+            JComboBox speedComboBox = (JComboBox) actionEvent.getSource();
+            speedString_Current = (String) speedComboBox.getSelectedItem();
+        });
+
         // Control
         // Up
         gripUpButton.addMouseListener(new MouseAdapter() {
@@ -292,7 +223,6 @@ public class GUI extends JPanel implements Network_iface, ActionListener {
                 network.writeSerial(1, temp);
             }
         });
-
         // Down
         gripDownButton.addMouseListener(new MouseAdapter() {
             @Override
@@ -310,7 +240,6 @@ public class GUI extends JPanel implements Network_iface, ActionListener {
                 network.writeSerial(1, temp);
             }
         });
-
         // Start
         startButton.addMouseListener(new MouseAdapter() {
             @Override
@@ -331,96 +260,121 @@ public class GUI extends JPanel implements Network_iface, ActionListener {
     }
 
     private void start() {
+        if(!Objects.equals(fileNameField.getText(), "")) {
+            if (!Objects.equals(fileLocation, "")) {
+                switch (testString_Current) {
+                    case "Tensile": {
+                        int temp[] = {'T'};
+                        network.writeSerial(1, temp);
+                        break;
+                    }
+                    case "Compression": {
+                        int temp[] = {'R'};
+                        network.writeSerial(1, temp);
+                        break;
+                    }
+                    default:
+                        System.out.println("Wrong Test input\n");
+                        break;
+                }
+                switch (forceString_Current) {
+                    case "100Kg": {
+                        int temp[] = {'E'};
+                        network.writeSerial(1, temp);
+                        break;
+                    }
+                    case "500Kg": {
+                        int temp[] = {'F'};
+                        network.writeSerial(1, temp);
+                        break;
+                    }
+                    default:
+                        System.out.println("Wrong Force input\n");
+                        break;
+                }
 
-//        if(Objects.equals(fileLocation, "")){
-//            System.out.println("No File location selected\n");
-//        }
-//        else {
-            switch (testString_Current) {
-                case "Tensile": {
-                    int temp[] = {'T'};
-                    network.writeSerial(1, temp);
-                    break;
+                switch (speedString_Current) {
+                    case "10 mm/min": {
+                        int temp[] = {'1'};
+                        network.writeSerial(1, temp);
+                        break;
+                    }
+                    case "50 mm/min": {
+                        int temp[] = {'2'};
+                        network.writeSerial(1, temp);
+                        break;
+                    }
+                    case "100 mm/min": {
+                        int temp[] = {'3'};
+                        network.writeSerial(1, temp);
+                        break;
+                    }
+                    default:
+                        System.out.println("Wrong Speed input\n");
+                        break;
                 }
-                case "Compression": {
-                    int temp[] = {'R'};
-                    network.writeSerial(1, temp);
-                    break;
-                }
-                default:
-                    System.out.println("Wrong Test input\n");
-                    break;
-            }
-            switch (forceString_Current) {
-                case "100Kg": {
-                    int temp[] = {'E'};
-                    network.writeSerial(1, temp);
-                    break;
-                }
-                case "500Kg": {
-                    int temp[] = {'F'};
-                    network.writeSerial(1, temp);
-                    break;
-                }
-                default:
-                    System.out.println("Wrong Force input\n");
-                    break;
-            }
 
-            switch (speedString_Current) {
-                case "10 mm/min": {
-                    int temp[] = {'1'};
-                    network.writeSerial(1, temp);
-                    break;
-                }
-                case "50 mm/min": {
-                    int temp[] = {'2'};
-                    network.writeSerial(1, temp);
-                    break;
-                }
-                case "100 mm/min": {
-                    int temp[] = {'3'};
-                    network.writeSerial(1, temp);
-                    break;
-                }
-                default:
-                    System.out.println("Wrong Speed input\n");
-                    break;
-            }
+                String fileName = fileNameField.getText();
+                System.out.println(fileLocation + "\\" + fileName + ".txt");
 
-//        if(!Objects.equals(fileNameField.getText(), "")) {
-//            fileName = fileNameField.getText();
-//        }
-//        else {
-        fileName = fileNameField.getText();
-        System.out.println(fileLocation + "\\" + fileName + ".txt");
-            try {
                 File textFile = new File(fileLocation + "\\" + fileName + ".txt");
-                FileOutputStream is = new FileOutputStream(textFile);
+                FileOutputStream is = null;
+                try {
+                    is = new FileOutputStream(textFile);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+                assert is != null;
                 OutputStreamWriter osw = new OutputStreamWriter(is);
                 Writer w = new BufferedWriter(osw);
-                w.write("POTATO!!!");
-                w.close();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
+                try {
+                    w.write("Start!");
 
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
 
-            //TODO: procedure loop!
-            // Check for cancel
-            if (stopNow || Objects.equals(elongation, "a")) {
-                int temp[] = { 'C' };
+                if (!stopNow || !Objects.equals(elongation, "a")) {
+                    // Start
+                    int temp[] = {'I'};
+                    network.writeSerial(1, temp);
+                }
+
+                try {
+                    w.write("Start!");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+//TODO: procedure loop!
+//        while (!stopNow || !Objects.equals(elongation, "a")) {
+//            try {
+//                Thread.sleep(500);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        }
+                // done or cancelled
+                int temp[] = {'C'};
                 network.writeSerial(1, temp);
                 // Stop timer
-
-
+                if (stopNow) {
+                    System.out.println("cancelled");
+                }
+                try {
+                    w.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
             } else {
-                // Start
-                int temp[] = { 'I' };
-                network.writeSerial(1, temp);
+                System.out.println("No File location selected\n");
             }
         }
+        else{
+            System.out.println("please input text");
+        }
 
+    }
 
     public void writeLog(int id, String text) {
         System.out.println("   log:  |" + text + "|");
@@ -440,67 +394,6 @@ public class GUI extends JPanel implements Network_iface, ActionListener {
         System.exit(0);
     }
 
-//    public void actionPerformed(ActionEvent e) {
-//
-//        //Handle open button action.
-//        if (e.getSource() == openButton) {
-//            int returnVal = fc.showOpenDialog(GUI.this);
-//
-//            if (returnVal == JFileChooser.APPROVE_OPTION) {
-//                File file = fc.getSelectedFile();
-//                fileLocation =fc.getSelectedFile();
-//
-//                log.append("Opening: " + file.getName() + "." + newline);
-//            } else {
-//                log.append("Open command cancelled by user." + newline);
-//            }
-//            log.setCaretPosition(log.getDocument().getLength());
-//            System.out.println("Current Path:");
-//            System.out.println(fc.getSelectedFile());
-//
-//            //Handle save button action.
-//        } else if (e.getSource() == saveButton) {
-//            int returnVal = fc.showSaveDialog(GUI.this);
-//            if (returnVal == JFileChooser.APPROVE_OPTION) {
-//                File file = fc.getSelectedFile();
-//                //This is where a real application would save the file.
-//                log.append("Saving: " + file.getName() + "." + newline);
-//            } else {
-//                log.append("Save command cancelled by user." + newline);
-//            }
-//            log.setCaretPosition(log.getDocument().getLength());
-//        }
-//    }
-
-//    /** Returns an ImageIcon, or null if the path was invalid. */
-//    private static ImageIcon createImageIcon(String path) {
-//        java.net.URL imgURL = FileChooser.class.getResource(path);
-//        if (imgURL != null) {
-//            return new ImageIcon(imgURL);
-//        } else {
-//            System.err.println("Couldn't find file: " + path);
-//            return null;
-//        }
-//    }
-
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method should be invoked from the
-     * event dispatch thread.
-     */
-//    private static void createAndShowGUI() {
-//        //Create and set up the window.
-//        JFrame frame = new JFrame("FileChooserDemo");
-//        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//
-//        //Add content to the window.
-//        frame.add(new GUI());
-//
-//        //Display the window.
-//        frame.pack();
-//        frame.setVisible(true);
-//    }
-
     private void createUIComponents() {
         String[] testStrings = {"Tensile", "Compression"};
         String[] forceStrings = {"100Kg", "500Kg"};
@@ -514,10 +407,6 @@ public class GUI extends JPanel implements Network_iface, ActionListener {
         fileNameField = new JTextField(20);
     }
 
-    @Override
-    public void actionPerformed(ActionEvent actionEvent) {
-        fileName = fileNameField.getText();
-    }
 }
 
 

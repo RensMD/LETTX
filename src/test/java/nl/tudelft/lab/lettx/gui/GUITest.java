bef8438d0700650c3dd9d2b538d9ad46c607e949
doorb02 <bert.doornbusch@gmail.com>
2016-07-07 20:25:25 +0200

    adding buildtool - trying Gradle, Maven is also used a lot by professionals reason - better and easier dependency management provides more flexible and quicker development. With a buildtool it is easy to get and update external libraries from a central repository. Configure once, run often.

---
+++ src/test/java/nl/tudelft/lab/lettx/gui/GUITest.java
@@ -0,0 +1,19 @@
+//package lettx;
+//
+//import org.junit.Test;
+//
+//import static org.junit.Assert.assertTrue;
+//
+///**
+// * Created by Bert on 22-6-2016.
+// */
+//public class GUITest {
+//
+//    @Test
+//    public void parseInput() {
+//        GUI gui = new GUI();
+//        int[] message = new int[]{1, 2, 3};
+//        gui.parseInput(1, 10, message);
+//        assertTrue(1 == message[0]);
+//    }
+//}

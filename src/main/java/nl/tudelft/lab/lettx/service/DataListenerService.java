e9f030c9faeb59f244495914c9975d318c1bf0dd
doorb02 <bert.doornbusch@gmail.com>
2016-07-24 01:41:15 +0200

    Handle input from Arduino with SerialComManagerDaoImpl  and extract lett nr after when test end signal is received

--- src/main/java/nl/tudelft/lab/lettx/service/DataListenerService.java
+++ src/main/java/nl/tudelft/lab/lettx/service/DataListenerService.java
@@ -6,8 +6,10 @@ import com.embeddedunveiled.serial.SerialComDataEvent;
 /**
  * Created by Rens on 22-7-2016.
  */
-public class DataListenerService implements ISerialComDataListener{
+public class DataListenerService implements ISerialComDataListener {
     boolean isDataReceived = false;
+    boolean isTestStartReceived = false;
+    boolean isTestEndReceived = false;
     StringBuilder message = new StringBuilder();
 
     @Override
@@ -16,7 +18,22 @@ public class DataListenerService implements ISerialComDataListener{
         isDataReceived = byteCount > 0;
         byte[] receivedData = dataEvent.getDataBytes();
         String receivedString = new String(receivedData);
+        message.append(receivedString);
+        isTestStartReceived = message.indexOf("I") > -1;
+        if (isTestStartReceived) {
+            isTestEndReceived = message.indexOf("C") > -1;
+            if (isTestEndReceived) {
+                int splitPosStart = message.indexOf("I") + 1;
+                int start = 0;
+                message.delete(0, splitPosStart);
+                String testMessage = message.toString();
+                String[] splitMessage = testMessage.split("/n");
+                String lettNr = splitMessage[0];
+                System.out.println("lett: " + lettNr);
+            }
+        }
         System.out.println("Message from Arduino: " + dataEvent.getDataBytesLength() + " byte(s): " + receivedString);
+        System.out.println("Message in Stringbuilder: " + message.toString());
     }
 
     @Override

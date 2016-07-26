b52dc5916a924816cc63d54ddaf918069c2c7724
RensMD <doornbuschrm@gmail.com>
2016-07-26 13:01:27 +0200

    c

---
+++ src/test/java/nl/tudelft/lab/lettx/util/MessageToTestdataConverterTest.java
@@ -0,0 +1,80 @@
+package nl.tudelft.lab.lettx.util;
+
+import nl.tudelft.lab.lettx.domain.TestResult;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+/**
+ * Created by Rens Doornbusch on 25-7-2016.
+ */
+public class MessageToTestdataConverterTest {
+    // test messages
+    public static final String NORMAL_LETT_TEST_MESSAGE = "TF4I19\n0.00\n0\n0\n3.10\n10.20\n1\n6.20\n20.40\n2\na\n";
+    private static final String EXTRA_END_CHARS_LETT_TEST_MESSAGE = "TF4I19\n0.00\n0\n0\n3.10\n10.20\n1\n6.20\n20.40\n2\na\n24.00\n12.34\n3\n";
+
+    MessageToTestdataConverter converter;
+
+    @Before
+    public void before() {
+        converter = new MessageToTestdataConverter();
+    }
+
+    @Test
+    public void splitTestExtractLettNumber() {
+        String[] splitMessage = converter.split(createNormalMessage());
+        Assert.assertEquals("19", splitMessage[0]);
+    }
+
+    @Test
+    public void splitTestExtractFirstData() {
+        String[] splitMessage = converter.split(createNormalMessage());
+        Assert.assertEquals("0.00", splitMessage[1]);
+    }
+
+    @Test
+    public void splitTestExtraCharactersAfterEnd() {
+        String[] splitMessage = converter.split(createExtraEndCharactersMessage());
+        int lastDataPos = splitMessage.length - 1;
+        Assert.assertEquals("2", splitMessage[lastDataPos]);
+    }
+
+    @Test
+    public void extractTesResults() {
+        String[] splitMessage = {"19", "1.60", "5.40", "0", "3.10", "10.20", "1", "6.20", "20.40", "2"};
+        List<TestResult> testResults = converter.convertTestResults(splitMessage);
+        Assert.assertEquals(3, testResults.size());
+    }
+
+    /**
+     * Message with characters after test end character "a"
+     *
+     * @return message
+     */
+    private StringBuilder createExtraEndCharactersMessage() {
+        return createMessage(EXTRA_END_CHARS_LETT_TEST_MESSAGE);
+    }
+
+    /**
+     * Normal message from Arduino Lett Test
+     *
+     * @return message
+     */
+    private StringBuilder createNormalMessage() {
+        return createMessage(NORMAL_LETT_TEST_MESSAGE);
+    }
+
+    /**
+     * Create test message.
+     *
+     * @param arduinoData
+     * @return message
+     */
+    private StringBuilder createMessage(String arduinoData) {
+        StringBuilder message = new StringBuilder();
+        return message.append(arduinoData);
+    }
+
+}
